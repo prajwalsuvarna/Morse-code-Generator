5ce0ca363b32aba06e63988951063d6a0305b8bb
NidheeshaT <ntbayari@gmail.com>
2022-07-27 15:08:54 +0530

    fixed the data grid and added box shadows,fixed the overflow of the header

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ ele.addEventListener("mousemove",(e)=>{
     let y=e.offsetY;//mouse y pos relative to element
     let valX=(x-(rect.width/2))/(rect.width/2);
     let valY=(y-(rect.height/2))/(rect.height/2);
-    ele.style.transform=`perspective(2000px) rotatey(${valX*15}deg) rotatex(${valY*15}deg)`;
+    ele.style.transform=`perspective(2000px) rotatey(${valX*10}deg) rotatex(${-valY*10}deg)`;
 })
 ele.addEventListener("mouseout",(e)=>{
     ele.style.transform=``;
