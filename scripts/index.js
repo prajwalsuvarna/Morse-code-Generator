050272526017714f420e2527675489dc8bea30da
decaffeinatedcoffee <77131841+decaffeinatedcoffee@users.noreply.github.com>
2022-10-02 04:18:25 -0300

    Added resources
    Added:
    Light mode
    Sound play
    Sound stop
    Vibration when light mode

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
-const ele=document.querySelector("header");
-
+var ele = document.querySelector("header");
 ele.addEventListener("mousemove",(e)=>{
     let rect=ele.getBoundingClientRect()//holds size and pos info the element
     let x=e.offsetX;//mouse x pos relative to element
@@ -25,6 +24,7 @@ const Enc=document.getElementById("eng-text");
 Enc.addEventListener("keyup", () => { 
 a=document.getElementById("eng-text").value.toUpperCase();
 a= a.split("");
+mcode=[];
 for (let x in a) {
   b=alp.indexOf(a[x]);
  mcode.push(morse_codes[b]);
@@ -32,7 +32,7 @@ for (let x in a) {
 mcode=mcode.toString().replace(/,/g, ' ');
 console.log(mcode);
 document.getElementById("mrs-cod").value=mcode;
-mcode=[];
+
 });
 
 //Decoding Part :
@@ -40,6 +40,7 @@ document.getElementById("mrs-cod").addEventListener("keyup", () => {
 a=document.getElementById("mrs-cod").value;
 a= a.split(" ");
 console.log(a);
+mcode=[];
 for (let x in a) {
   b=morse_codes.indexOf(a[x]);
  mcode.push(alp[b]);
@@ -47,7 +48,75 @@ for (let x in a) {
 mcode=mcode.toString().replace(/,/g, '');
 console.log(mcode);
 document.getElementById("eng-text").value=mcode;
-mcode=[];
+
 });
 
+//Audio Function
+
+
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+var dot = 1.2 / 15;
+var time = context.currentTime;
+var oscillator = context.createOscillator();
+
+function play(){
+  oscillator.type = "sine";
+  oscillator.frequency.value = 600;
+
+  var gainNode = context.createGain();
+  gainNode.gain.setValueAtTime(0, time);
+
+  mcode.split("").forEach(function(x) {
+      if(x == ".") {
+              gainNode.gain.setValueAtTime(1, time);
+              time += dot;
+              gainNode.gain.setValueAtTime(0, time);
+              time += dot;
+         }else if(x == "-"){
+              gainNode.gain.setValueAtTime(1, time);
+              time += 3 * dot;
+              gainNode.gain.setValueAtTime(0, time);
+              time += dot;
+         }else if(x == " "){
+           time += 7 * dot;
+          }
+      });
+  oscillator.connect(gainNode);
+  gainNode.connect(context.destination);
+  oscillator.start();
+}
+
+function stop(){
+  oscillator.stop();
+}
+
 //Light flash Functionality
+
+let curr = 0;
+function blink(){
+if(curr < mcode.length){
+  
+  if(mcode[curr] == "-"){
+  document.getElementById("light").style.backgroundColor = "red";
+  navigator.vibrate(500);
+  setTimeout(() => {
+    document.getElementById("light").style.backgroundColor = "black";
+  }, 500);
+  setTimeout(blink, 700);
+  }else if(mcode[curr] == "."){
+    document.getElementById("light").style.backgroundColor = "blue";
+    navigator.vibrate(300);
+    setTimeout(() => {
+      document.getElementById("light").style.backgroundColor = "black";
+    }, 300);
+    setTimeout(blink, 500);   
+ }else if(mcode[curr] == " "){
+  setTimeout(blink, 700);   
+ }
+ curr++;
+}else{
+  curr = 0;
+}
+
+}
