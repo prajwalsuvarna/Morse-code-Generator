5c31f37dd0665822233908479115d78b41c0e7ef
Hunt <prajwalsuvarna1906@gmail.com>
2022-12-26 15:39:00 +0530

    Bug fix

--- scripts/index.js
+++ scripts/index.js
@@ -22,7 +22,6 @@ let mcode=[];
 
 // Encoding part :
 const Enc=document.getElementById("eng-text");
-Enc.focus() = true;
 Enc.addEventListener("keyup", () => { 
 a=document.getElementById("eng-text").value.toUpperCase();
 a= a.split("");
